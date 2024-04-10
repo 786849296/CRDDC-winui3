fe3311757a2ecc4afc553e84791d8c16b79b4f1e
koloxiya <z786849296@outlook.com>
2024-04-10 21:59:26 +0800

    v1.0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,21 @@
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,14 +27,90 @@ namespace CRDDC
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        readonly YPLNet model = new("Assets\\32-1280-140.onnx");
+        Images target;
+
         public MainWindow()
         {
             this.InitializeComponent();
+            this.ExtendsContentIntoTitleBar = true;
+            this.SetTitleBar(AppTitleBar);
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void border_image_DragEnter(object sender, DragEventArgs e)
         {
-            myButton.Content = "Clicked";
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+                e.AcceptedOperation = DataPackageOperation.Copy;
+            else
+                e.AcceptedOperation = DataPackageOperation.None;
+        }
+        private async void border_image_Drop(object sender, DragEventArgs e)
+        {
+            var border = sender as Border;
+            var items = await e.DataView.GetStorageItemsAsync();
+            var storageFile = items[0] as StorageFile;
+            string[] imageExtensions = [".jpg", ".jpeg", ".png"];
+            if (!imageExtensions.Any(ext => storageFile.FileType == ext))
+                return;
+            target = new(storageFile.Path);
+            BitmapImage bitmap = new();
+            bitmap.SetSource(await storageFile.OpenAsync(FileAccessMode.Read));
+            target.imageHeight = bitmap.PixelHeight;
+            target.imageWidth = bitmap.PixelWidth;
+            if (border.Child is not Canvas)
+            {
+                border.Child = new Canvas();
+                var canvas = border.Child as Canvas;
+                canvas.Children.Add(new Image());
+                (canvas.Children[0] as Image).Stretch = Stretch.Uniform;
+                Canvas.SetZIndex(canvas.Children[0], 0);
+                toggleButton_start.IsEnabled = true;
+            }
+            var image = (border.Child as Canvas).Children[0] as Image;
+            image.Width = border.ActualWidth;
+            image.Height = border.ActualHeight;
+            image.Source = bitmap;
+            for (int i = (border.Child as Canvas).Children.Count - 1; i > 0; i--)
+                (border.Child as Canvas).Children.RemoveAt(i);
+            toggleButton_start.IsChecked = false;
+        }
+
+        private void toggleButton_start_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)(sender as ToggleButton).IsChecked)
+            {
+                var results = model.predict(target);
+                var canvas = border_image.Child as Canvas;
+                foreach (var result in results)
+                {
+                    canvas.Children.Add(new Border());
+                    var anchor = canvas.Children.Last() as Border;
+                    var zoom = canvas.ActualHeight / target.imageHeight;
+                    var imageActualWidth = target.imageWidth * zoom;
+                    result.x1 *= (float)zoom;
+                    result.x2 *= (float)zoom;
+                    result.y1 *= (float)zoom;
+                    result.y2 *= (float)zoom;
+                    var x = (canvas.ActualWidth - imageActualWidth) / 2;
+                    Canvas.SetLeft(anchor, x + result.x1);
+                    Canvas.SetTop(anchor, result.y1);
+                    Canvas.SetZIndex(anchor, 1);
+                    anchor.Width = result.x2 - result.x1;
+                    anchor.Height = result.y2 - result.y1;
+                    anchor.BorderBrush = new SolidColorBrush(result.getBoxColor());
+                    anchor.BorderThickness = new Thickness(2);
+
+                    canvas.Children.Add(new Border());
+                    var label = canvas.Children.Last() as Border;
+                    Canvas.SetLeft(label, x + result.x1);
+                    Canvas.SetTop(label, result.y1 - 20);
+                    Canvas.SetZIndex(label, 1);
+                    label.Background = new SolidColorBrush(result.getBoxColor());
+                    label.Child = new TextBlock();
+                    (label.Child as TextBlock).Foreground = new SolidColorBrush(Colors.White);
+                    (label.Child as TextBlock).Text = $"{result.confidence:f2} {result.className})";
+                }
+            }
         }
     }
 }
---
+++ YPLNet.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace CRDDC
+{
+    public class Images(string path)
+    {
+        [ColumnName("images")]
+        public string imagePath = path;
+
+        public float imageWidth;
+        public float imageHeight;
+    }
+
+    public class Output
+    {
+        [ColumnName("output")]
+        public float[] labels;
+    }
+
+    public class Anchor
+    {
+        public const int classNum = 4;
+        public static readonly string[] classes = ["D00", "D10", "D20", "D30"];
+
+        private static readonly Dictionary<string, Color> classToColor = new() 
+        {
+            { "D00", Colors.Red },
+            { "D10", Colors.Pink },
+            { "D20", Colors.Orange },
+            { "D30", Colors.Yellow }
+        };
+
+        public float x1;
+        public float y1;
+        public float x2;
+        public float y2;
+        public float confidence;
+        public string className;
+
+        public static float IOU(Anchor a, Anchor b)
+        {
+            var areaA = (a.x2 - a.x1) * (a.y2 - a.y1);
+            var areaB = (b.x2 - b.x1) * (b.y2 - b.y1);
+            var minX = Math.Max(a.x1, b.x1);
+            var minY = Math.Max(a.y1, b.y1);
+            var maxX = Math.Min(a.x2, b.x2);
+            var maxY = Math.Min(a.y2, b.y2);
+            var intersectionArea = Math.Max(maxX - minX, 0) * Math.Max(maxY - minY, 0);
+            return intersectionArea / (areaA + areaB - intersectionArea);
+        }
+
+        // Convert from yolov5-master/utils/general.py/non_max_suppression()
+        // But according to the only steps predicted by YPL, the code can be deleted and cut
+        public static List<Anchor> NMS(Output pred, double conf_thres, double iou_thres, int max_det = 300)
+        {
+            List<int> confI = [];
+            for (int i = 4; i < pred.labels.Length; i += 9)
+                if (pred.labels[i] > conf_thres)
+                    confI.Add(i);
+            List<Anchor> output = [];
+            foreach (int i in confI)
+            {
+                float[] classConf = [pred.labels[i + 1], pred.labels[i + 2], pred.labels[i + 3], pred.labels[i + 4]];
+                Anchor anchor = new()
+                {
+                    x1 = pred.labels[i - 4] - pred.labels[i - 2] / 2,
+                    y1 = pred.labels[i - 3] - pred.labels[i - 1] / 2,
+                    x2 = pred.labels[i - 4] + pred.labels[i - 2] / 2,
+                    y2 = pred.labels[i - 3] + pred.labels[i - 1] / 2,
+                    confidence = pred.labels[i] * classConf.Max(),
+                    className = classes[Array.IndexOf(classConf, classConf.Max())]
+                };
+                if (anchor.confidence < conf_thres || anchor.x2 - anchor.x1 < 10 || anchor.y2 - anchor.y1 < 10)
+                    continue;
+                output.Add(anchor);
+            }
+            // Convert from https://zh.d2l.ai/chapter_computer-vision/anchor.html#subsec-predicting-bounding-boxes-nms
+            output.Sort((a, b) => b.confidence.CompareTo(a.confidence));
+            for (int i = 1; i < Math.Min(output.Count, max_det); i++)
+                for (int j = 0; j < i; j++)
+                    if (IOU(output[i], output[j]) > iou_thres)
+                    {
+                        output.RemoveAt(i);
+                        i--;
+                        break;
+                    }
+            return output.GetRange(0, Math.Min(output.Count, max_det));
+        }
+
+        public Color getBoxColor()
+        {
+            if (classToColor.TryGetValue(className, out Color color))
+                return color;
+            else
+                throw new ArgumentException("Invalid class name.");
+        }
+    }
+
+    class YPLNet : IDisposable
+    {
+        private readonly MLContext mlContext = new();
+        public PredictionEngine<Images, Output> predictor;
+
+        public static string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+            return fullPath;
+        }
+
+        public YPLNet(string modelLocation)
+        {
+            var data = mlContext.Data.LoadFromEnumerable(new List<Images>());
+            var pipeline = mlContext.Transforms.LoadImages("images", "", "images")
+                .Append(mlContext.Transforms.ResizeImages("images", 1280, 1280, "images", Microsoft.ML.Transforms.Image.ImageResizingEstimator.ResizingKind.IsoPad))
+                //真的想给你两坨子，scaleImage, 一点都不明确
+                .Append(mlContext.Transforms.ExtractPixels("images", scaleImage: 1 / 255f))
+                .Append(mlContext.Transforms.ApplyOnnxModel(GetAbsolutePath(modelLocation)));
+            var model = pipeline.Fit(data);
+            predictor = mlContext.Model.CreatePredictionEngine<Images, Output>(model);
+        }
+
+        public List<Anchor> predict(Images x, int imgsz = 1280)
+        {
+            var output = predictor.Predict(x);
+            var anchors = Anchor.NMS(output, 0.09, 0.5);
+            foreach (var anchor in anchors)
+            {
+                var gain = Math.Min(x.imageWidth / imgsz, x.imageHeight / imgsz);
+                var padW = (x.imageWidth - imgsz * gain) / 2;
+                var padH = (x.imageHeight - imgsz * gain) / 2;
+                anchor.x1 = (anchor.x1 - padW) * gain;
+                anchor.x2 = (anchor.x2 - padW) * gain;
+                anchor.y1 = (anchor.y1 - padH) * gain;
+                anchor.y2 = (anchor.y2 - padH) * gain;
+
+                anchor.x1 = Math.Max(0, Math.Min(anchor.x1, x.imageWidth));
+                anchor.x2 = Math.Max(0, Math.Min(anchor.x2, x.imageWidth));
+                anchor.y1 = Math.Max(0, Math.Min(anchor.y1, x.imageHeight));
+                anchor.y2 = Math.Max(0, Math.Min(anchor.y2, x.imageHeight));
+            }
+            return anchors;
+        }
+
+        public void Dispose()
+        {
+            predictor.Dispose();
+        }
+    }
+}
